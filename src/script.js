865c7ac58f0236bdd7ba83e47bd5b0ee71ddc0f4
babisatarova@gmail.com <babisatarova@gmail.com>
2025-07-21 18:26:14 +0200

    lämna in

--- src/script.js
+++ src/script.js
@@ -26,21 +26,25 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
-  
+  let groupToShow = "familj";
 
-
-
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
-
-
+  function showContacts(contactBook, groupToShow) {
+    // Rör ej denna kod
 
     // Loopar igenom alla kontakter och jämför grupp
    
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+    for (contact of contactBook.contacts) {
+      if (contact.group === groupToShow) {
 
+        return `Namn: ${contact.name}, Telefon: ${contact.phone}`;  
+        
+        // här ska sträng med namn och telefonnummer visas i form av string interpolation
+      
+      
+      }     
+    }
+  }
 
-}
 
 
 // Rör ej denna kod
